5783431b54ac3e7661b6f869453bdbbe419351f3
lizarzoro <tkddnr468@gmail.com>
2019-05-07 13:23:20 +0900

    UFO Game

---
+++ New UFO Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    public float speed;
+    public Text countText;
+    private int count;
+    public Text winText;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2d.AddForce(movement * speed);
+    }
+
+    //public bool characterInQuicksand;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //characterInQuicksand = true;
+        //GameObject
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+    void SetCountText()
+    {
+        countText.text = "Count : " + count.ToString();
+        if (count >= 7)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
